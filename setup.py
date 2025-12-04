fce44db3d02bf735efacefa3fb6c5ae25caaeb2e
Henri J. Norden <55378880+Henri-J-Norden@users.noreply.github.com>
2025-12-04 20:48:05 +0200

    Suffix version with +newjson

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ github_url = 'https://github.com/mymarilyn/clickhouse-driver'
 
 setup(
     name='clickhouse-driver',
-    version=read_version(),
+    version=read_version() + "+newjson",
 
     description='Python driver with native interface for ClickHouse',
     long_description=long_description,
